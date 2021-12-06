c8b462a0e5ee22794c1f85a4eb7e9a09643e51cd
azerahmedov <ehmedovh111@gmail.com>
2021-12-06 22:50:03 +0400

    duzenlemeler

--- asetts/js/main.js
+++ asetts/js/main.js
@@ -1,12 +1,28 @@
-let swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper", {
     spaceBetween: 10,
     slidesPerView: 10,
     freeMode: true,
     watchSlidesProgress: true,
+    breakpoints: {
+      300:{
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 25,
+      },
+      1023: {
+        slidesPerView: 10,
+        spaceBetween: 10,
+      },
+    },
   });
-let swiper2 = new Swiper(".mySwiper2", {
+  
+var  swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
     thumbs: {
       swiper: swiper,
     },
-  });
+});
+
